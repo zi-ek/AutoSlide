88cda6ae210ad654e8f911ca93be8c4fd766e2c7
Administrator <admin@localhost>
2026-08-16 00:30:26 +0800

    动作列表底部按钮改为3:1布局（导入配置3份、取消1份）

--- app/src/main/java/com/ziek/autoslide/service/FloatingWindowService.kt
+++ app/src/main/java/com/ziek/autoslide/service/FloatingWindowService.kt
@@ -585,7 +585,7 @@ class FloatingWindowService : Service() {
             gravity = Gravity.CENTER_VERTICAL
             addView(
                 importButton,
-                LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1f)
+                LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 3f)
             )
             addView(
                 cancelButton,
